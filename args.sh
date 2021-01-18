18714b84d1676fa877a548b87b0f3a4c9c6ee429
root <pp@example.com>
2021-01-18 14:26:42 +0800

    shell脚本的参数

--- args.sh
+++ args.sh
@@ -4,11 +4,14 @@ echo 'hello world'
 echo $0
 echo $1
 echo $2
-if [ $1 -eq $2 ];then
-	echo `$1 = $2`
-elif [ $1 -gt $2 ];then
-	 echo `$1 > $2`
-else 
-         echo `$a < $b`
-fi
+
+#  运行的时候输入下列代码:
+#  前边表示脚本的名字，5和6表示传入参数
+#  ./args.sh 5 6
+
+
+# 运行结果:
+# ./args.sh   这一行是$0的输出，$0获取脚本的名称
+# 5           $1和$2是用来获取输入的参数
+# 6
 
